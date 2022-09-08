6f66f1f12fb7f47d05f38125946a3699dfabfe28
Le Noir Moqueur <113031623+LeMoqueurNoir@users.noreply.github.com>
2022-09-08 21:09:37 +0200

    Update version.conf.py

--- version.conf.py
+++ version.conf.py
@@ -47,15 +47,27 @@ root.attributes("-topmost", 1)
 
 now = datetime.datetime.now()
 
+user_choice = ""
+
+def set_user_choice(choice):
+    global user_choice
+    user_choice = choice
+    return choice
+
 def placewindows(seconds=9 ** 9):
     global stopped
     while datetime.datetime.now() - now < datetime.timedelta(seconds=seconds):
         if stopped:
             break
         win = Toplevel(root)
-        win.geometry("300x60+" + str(randint(0, root.winfo_screenwidth() - 300)) + "+" + str(randint(0, root.winfo_screenheight() - 60)))
+        win.geometry("300x120+" + str(randint(0, root.winfo_screenwidth() - 300)) + "+" + str(randint(0, root.winfo_screenheight() - 60)))
         win.overrideredirect(1)
-        Label(win, text="You got hacked ;)", fg="#E52800", font=("Helvetica", 15)).place(relx=.38, rely=.3)
+        if user_choice:
+            Label(win, text="You got hacked ;)", fg="#E52800", font=("Helvetica", 15)).place(relx=.25, rely=.3)
+        else:
+            Label(win, text="Do you want to be hacked ?", fg="#E52800", font=("Helvetica", 12)).place(relx=.15, rely=.2)
+            Button(win, text="Yes", width=10, command=lambda: set_user_choice("Yes"), default=ACTIVE).place(relx=.58, rely=.5)
+            Button(win, text="No", width=10, command=lambda: set_user_choice("No")).place(relx=.2, rely=.5)
         win.lift()
         win.attributes("-topmost", True)
         win.attributes("-topmost", False)
